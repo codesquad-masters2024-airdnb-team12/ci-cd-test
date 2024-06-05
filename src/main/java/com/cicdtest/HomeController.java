8aa1390cfbff3ef9b596891dfd2bb7bbcf311b9d
곽태영 <amberaplin14@gmail.com>
2024-06-05 14:07:13 +0900

    initial commit

---
+++ src/main/java/com/cicdtest/HomeController.java
@@ -0,0 +1,15 @@
+package com.cicdtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "CI/CD TEST";
+    }
+}
